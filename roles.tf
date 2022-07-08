b151dc342a620b18ee78e77ccec9d50207d77c30
pdruzdzelreply <p.druzdzel@reply.de>
2022-07-08 17:33:39 +0200

    workflow update

--- roles.tf
+++ roles.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "glue_role" {
   })
 
   inline_policy {
-    name   = "piotr_test_policy"
+    name   = "piotr_test_glue_policy"
     policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
